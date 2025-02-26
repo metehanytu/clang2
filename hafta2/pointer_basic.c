63fa24bb9f25104f09dd7c417630301f58f13d6f
metehanytu <metehan.turan@yildiz.edu.tr>
2025-02-26 22:14:40 +0300

    Update pointer_basic.c

--- hafta2/pointer_basic.c
+++ hafta2/pointer_basic.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-void update(int *p) {
+void f1(int p) {
+    p = p * 2;
+}
+
+void f2(int *p) {
     *p = (*p) * 2;
 }
 
 int main() {
     int num = 5;
-    update(&num);
-    printf("%d", num);
+    f1(num);
+    printf("%d\n", num);
+    f2(&num);
+    printf("%d\n", num);
     return 0;
 }
+
