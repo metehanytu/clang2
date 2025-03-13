6a9d4f189902add27de52c77069cf5fd615e0b4a
metehanytu <metehan.turan@yildiz.edu.tr>
2025-03-13 14:11:51 +0300

    Create problem4.c

---
+++ hafta4/problems/problem4.c
@@ -0,0 +1,29 @@
+# Recursive Prime Factorization
+
+## **Problem Statement**
+Write a C program that:
+- Takes an integer `n` as input from the user.
+- Uses **recursion** to find and print all **prime factors** of `n`.
+- Dynamically allocates memory using `calloc()` to store the factors.
+- Properly frees allocated memory after use.
+
+## **Expected Input/Output**
+### **Example 1:**
+#### **Input:**
+```
+Enter a number: 56
+```
+#### **Output:**
+```
+Prime factors of 56: 2 2 2 7
+```
+
+### **Example 2:**
+#### **Input:**
+```
+Enter a number: 75
+```
+#### **Output:**
+```
+Prime factors of 75: 3 5 5
+```
